42f5e3a173960d39cb2bf979a24c9082bc0b3f3a
Claudio Gil <claudio.f.gil@gmail.com>
2018-03-03 00:01:22 +0000

    Adapted success page to translation

--- html/success/index.php
+++ html/success/index.php
@@ -1,17 +1,18 @@
 <?php $config = include('../_config.php'); ?>
+<?php include('../_tx.php'); ?>
 
 <!DOCTYPE html>
-<html lang="en" prefix="og: http://ogp.me/ns#">
+<html lang="<?= $lang ?>" prefix="og: http://ogp.me/ns#">
   <head>
     <?php
     $headInfo = [
-      'title'       => 'Form Sumbitted Succesfully',
-      'description' => '',
+      'title'       => tx('success_title'),
+      'description' => tx('success_description'),
       'url'         => '',
       'og_img'      => 'https://thebitcoincash.fund/assets/img/bcf_opengraph.jpg'
     ]; ?>
     <?php include($config['include_dir'] . 'head.php'); ?>
-    <title><?= $headInfo['title'] . $config['title_post'] ?></title>
+    <title><?= tx($config['current_page'] . '_title') . tx('title_post') ?></title>
   </head>
   <body>
     <?php include($config['include_dir'] . 'nav.php'); ?>
@@ -26,8 +27,8 @@
             <source srcset="<?= $config['svg_dir']; ?>success_hero_mobile.svg" />
             <img src="<?= $config['svg_dir']; ?>success_hero_desktop.svg" class="img-responsive hero-img" />
           </picture>
-          <h1 class="hero-heading">Form Submitted succesfully!</h1>
-          <p class="hero-lead">Thanks for reaching out.</p>
+          <h1 class="hero-heading"><?= tx('success_heading') ?></h1>
+          <p class="hero-lead"><?= tx('success_lead') ?></p>
         </div>
       </div>
       <div class="successChat">
@@ -36,10 +37,9 @@
             <div class="col-md-5 col-md-push-7 successChat-imgCol">
               <img src="<?= $config['svg_dir']; ?>success_chat.svg" class="img-responsive successChat-img"></div>
             <div class="col-md-7 col-md-pull-5 successChat-txtCol">
-              <h3 class="successChat-heading">Join Our Chat</h3>
-              <p>While waiting for a reply to your submission, why not join the Bitcoin Cash Fund public chat group? This is where we discuss projects, proposals and all things Bitcoin.</p>
-              <p>We're a friendly bunch and we all have one shared mission: to introduce the world to Bitcoin Cash!</p>
-              <a href="https://chat.thebitcoincash.fund/" target="_blank" class="btn btn-lg successChat-btn">Join Us!</a>
+    <h3 class="successChat-heading"><?= tx('success_chat_title') ?></h3>
+              <?= tx('success_chat_text') ?>
+              <a href="https://chat.thebitcoincash.fund/" target="_blank" class="btn btn-lg successChat-btn"><?= tx('success_chat_join_button') ?></a>
             </div>
           </div>
         </div>
